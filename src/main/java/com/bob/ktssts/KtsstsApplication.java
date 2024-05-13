7e8bf4b893c382c06fce5b8b9eb88caa3458b66b
apprentice <258997732@qq.com>
2024-05-13 16:29:06 +0800

    first created

---
+++ src/main/java/com/bob/ktssts/KtsstsApplication.java
@@ -0,0 +1,13 @@
+package com.bob.ktssts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtsstsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtsstsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bob/ktssts/KtsstsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.ktssts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtsstsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
