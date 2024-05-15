f50c2c4306fa7cc1958c065f14706d988ebb221d
apprentice <258997732@qq.com>
2024-05-15 14:52:50 +0800

    增加xxl-job支持

--- src/main/java/com/bob/ktssts/api/controller/WebController.java
+++ src/main/java/com/bob/ktssts/api/controller/WebController.java
@@ -1,18 +1,12 @@
 package org.inlighting.controller;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.*;
 import org.apache.shiro.subject.Subject;
-import org.inlighting.bean.ResponseBean;
-import org.inlighting.database.UserService;
-import org.inlighting.database.UserBean;
-import org.inlighting.exception.UnauthorizedException;
-import org.inlighting.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 public class WebController {
